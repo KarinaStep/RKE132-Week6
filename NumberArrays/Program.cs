afd183ab37145a12a89eda869c152b5cd1d695b1
Karina <karinastepp@gmail.com>
2022-10-08 17:53:18 +0300

    add week6StringAndArrays code

---
+++ NumberArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = new int[10];
+Random rnd = new Random();
+
+int sum = 0;
+
+//populating the array ehk salvestame väärtused massiivi
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(1, numbers.Length + 1);
+    sum = sum + numbers[i];
+}
+
+//sellega kuvan numbrid, mida ta salvestas for loopi käigus
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine($"Total: {sum}");
